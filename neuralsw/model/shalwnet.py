ef0685f64f8ec2ac594ec93da001c80804dd0ff1
brajard <julien.brajard@upmc.fr>
2018-05-18 16:59:28 +0200

    adapt nn shalw to new sw class

--- neuralsw/model/shalwnet.py
+++ neuralsw/model/shalwnet.py
@@ -1,14 +1,14 @@
-from shalw import SWmodel
-from modeltools import loadmymodel
-from datatools import MakeSmallImages, MakeBigImage,  MakeSmallImages_ind
+from .shalw import SWmodel
+from .modeltools import loadmymodel
+from .datatools import MakeSmallImages, MakeBigImage,  MakeSmallImages_ind
 import numpy as np
 from scipy.ndimage import gaussian_filter
 
 class SWparnn(SWmodel):
 	def __init__ (self, nnupar,nnvpar,nout=(1,1),delta=(1,1),
-			f0=3.5e-5, beta=2.11e-11, gamma=2e-7, gstar=0.02,rho0=1000, H=500, taux0=0.15, tauy0=0, nu=0.72, dt=1800,
-			dx=20e3, dy=20e3, alpha=0.025, nx=80, ny=80 ):
-		SWmodel.__init__(self,f0, beta, gamma, gstar,rho0, H, taux0, tauy0, nu, dt,
+			f0=3.5e-5, beta=2.11e-11, gamma=2e-7, gstar=0.02,
+			rho0=1000, H=500, fwind=None, warg=dict(), nu=0.72, dt=1800, dx=20e3, dy=20e3, alpha=0.025, nx=80, ny=80):
+		SWmodel.__init__(self,f0, beta, gamma, gstar,rho0, H, fwind, warg, nu, dt,
 			dx, dy, alpha, nx, ny )
 
 		if isinstance(nnupar, str):
@@ -43,10 +43,9 @@ class SWparnn(SWmodel):
 		self.vparam = MakeBigImage(y,ind=self._indout)
 
 class SWparnnim(SWmodel):
-	def __init__ (self, nnupar='',nnvpar='',
-			f0=3.5e-5, beta=2.11e-11, gamma=2e-7, gstar=0.02,rho0=1000, H=500, taux0=0.15, tauy0=0, nu=0.72, dt=1800,
-			dx=20e3, dy=20e3, alpha=0.025, nx=80, ny=80 ):
-		SWmodel.__init__(self,f0, beta, gamma, gstar,rho0, H, taux0, tauy0, nu, dt,
+	def __init__ (self, nnupar='',nnvpar='', f0=3.5e-5, beta=2.11e-11, gamma=2e-7, gstar=0.02, rho0=1000, H=500,
+			fwind=None, warg={ }, nu=0.72, dt=1800, dx=20e3, dy=20e3, alpha=0.025, nx=80, ny=80 ):
+		SWmodel.__init__(self,f0, beta, gamma, gstar,rho0, H, fwind, warg, nu, dt,
 			dx, dy, alpha, nx, ny )
 
 		if isinstance(nnupar, str):
@@ -91,10 +90,9 @@ class SWparnnim(SWmodel):
 
 
 class SWparnnhdyn(SWmodel):
-	def __init__ (self, nnupar='',nnvpar='',nnhdyn='',
-			f0=3.5e-5, beta=2.11e-11, gamma=2e-7, gstar=0.02,rho0=1000, H=500, taux0=0.15, tauy0=0, nu=0.72, dt=1800,
-			dx=20e3, dy=20e3, alpha=0.025, nx=80, ny=80 ):
-		SWmodel.__init__(self,f0, beta, gamma, gstar,rho0, H, taux0, tauy0, nu, dt,
+	def __init__ (self, nnupar='',nnvpar='',nnhdyn='', f0=3.5e-5, beta=2.11e-11, gamma=2e-7, gstar=0.02, rho0=1000,
+			H=500, fwind=None, warg={ }, nu=0.72, dt=1800, dx=20e3, dy=20e3, alpha=0.025, nx=80, ny=80 ):
+		SWmodel.__init__(self,f0, beta, gamma, gstar,rho0, H, fwind, warg, nu, dt,
 			dx, dy, alpha, nx, ny )
 
 		if isinstance(nnupar, str):
